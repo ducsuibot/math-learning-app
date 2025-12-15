9fa6d82d94b0f11f97e7e65b0b7c2355f02f45d9
ducsuibot <soisamsit@example.com>
2025-12-15 23:01:25 +0700

    fix

--- app.py
+++ app.py
@@ -625,8 +625,8 @@ def submit_mission():
 # --------------------------------------------------------
 # ... (giữ nguyên các route cũ)
 # === 9. CHẠY SERVER (ĐÃ VÔ HIỆU HÓA ĐỂ DEPLOY) ===
-if __name__ == '__main__':
-     with app.app_context():
-         db.create_all() # Tạo bảng nếu chưa có
-     print("Khởi động server...")
-     app.run(debug=True, port=5000)
+#if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all() # Tạo bảng nếu chưa có
+#     print("Khởi động server...")
+#     app.run(debug=True, port=5000)
