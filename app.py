5037a641798c759adab2790d2ede584fcb058ffe
ducsuibot <soisamsit@example.com>
2025-12-30 10:00:57 +0700

    ok

--- app.py
+++ app.py
@@ -501,15 +501,15 @@ def send_feedback():
     return redirect(url_for('parent_dashboard'))
 #end 30/12
 
-#if __name__ == '__main__':
-#    with app.app_context():
-#        db.create_all()
-#        # Tạo admin mẫu nếu chưa có
-#        if not User.query.filter_by(username='admin').first():
-#            admin = User(username='admin', email='admin@doraemon.com', role='admin')
-#            admin.set_password('admin123')
-#            db.session.add(admin)
-#            db.session.commit()
-#            print(">>> Đã tạo tài khoản admin mẫu (User: admin / Pass: admin123)")
-#    print(">>> Khởi động server...")
-#    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        # Tạo admin mẫu nếu chưa có
+        if not User.query.filter_by(username='admin').first():
+            admin = User(username='admin', email='admin@doraemon.com', role='admin')
+            admin.set_password('admin123')
+            db.session.add(admin)
+            db.session.commit()
+            print(">>> Đã tạo tài khoản admin mẫu (User: admin / Pass: admin123)")
+    print(">>> Khởi động server...")
+    app.run(debug=True, port=5000)
